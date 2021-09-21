9624e58009b5ecced1a9b55b2b86954e05beb4cd
yoonminsang <apdlyooapdl@naver.com>
2021-09-22 01:34:16 +0900

    프로그래머스 고득점 키트 2ㅜ문제

---
+++ programmers/kit/hash.js
@@ -0,0 +1,8 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42576
+function solution(participant, completion) {
+  participant.sort();
+  completion.sort();
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) return participant[i];
+  }
+}
---
+++ programmers/kit/stack-queue.js
@@ -0,0 +1,16 @@
+function solution(progresses, speeds) {
+  const days = progresses.map((progress, idx) =>
+    Math.ceil((100 - progress) / speeds[idx])
+  );
+  const answer = [];
+  while (days.length) {
+    const standard = days.shift();
+    let count = 1;
+    while (days.length && days[0] <= standard) {
+      days.shift();
+      count++;
+    }
+    answer.push(count);
+  }
+  return answer;
+}
