8520496aa22c68304870a62a39a24ae84cbf926c
yoonminsang <apdlyooapdl@naver.com>
2021-09-30 14:40:11 +0900

    완전탐색-소수찾기

--- programmers/kit/full-navigation.js
+++ programmers/kit/full-navigation.js
@@ -23,3 +23,35 @@ function solution(answers) {
   }
   return answer;
 }
+
+const getPermutations = (arr, selectNumber) => {
+  if (selectNumber === 1) return arr.map((v) => [v]);
+  const result = [];
+  arr.forEach((v, i) => {
+    const rest = arr.slice(0, i).concat(arr.slice(i + 1));
+    const permutations = getPermutations(rest, selectNumber - 1);
+    const attached = permutations.map((permutation) => [v, ...permutation]);
+    result.push(...attached);
+  });
+  return result;
+};
+
+const isPrime = (number) => {
+  if (number < 2) return false;
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) return false;
+  }
+  return true;
+};
+
+// https://programmers.co.kr/learn/courses/30/lessons/42839
+function solution(numbers) {
+  let permutations = [];
+  const numbersArr = numbers.split('');
+  for (let i = 1; i <= numbers.length; i++) {
+    permutations.push(...getPermutations(numbersArr, i));
+  }
+  permutations = permutations.map((v) => +v.join(''));
+  permutations = [...new Set(permutations)];
+  return permutations.reduce((acc, cur) => (isPrime(cur) ? acc + 1 : acc), 0);
+}
