da9c052915bf0e60847d4febbe4fc8a32750dcfe
yoonminsang <apdlyooapdl@naver.com>
2021-09-30 13:47:53 +0900

    정렬-가장큰수

--- programmers/kit/sort.js
+++ programmers/kit/sort.js
@@ -14,3 +14,12 @@ function solution(array, commands) {
   });
   return answer;
 }
+
+// https://programmers.co.kr/learn/courses/30/lessons/42746
+function solution(numbers) {
+  const answer = numbers
+    .map((number) => String(number))
+    .sort((a, b) => b + a - (a + b))
+    .join('');
+  return answer[0] === '0' ? '0' : answer;
+}
