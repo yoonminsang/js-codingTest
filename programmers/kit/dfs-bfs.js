477fad62ad550daa9c5bc35c260fcc69e43f2b48
yoonminsang <apdlyooapdl@naver.com>
2021-10-02 17:25:54 +0900

    dfs-네트워크

--- programmers/kit/dfs-bfs.js
+++ programmers/kit/dfs-bfs.js
@@ -12,3 +12,24 @@ function solution(numbers, target) {
   dfs(0, 0);
   return count;
 }
+
+// https://programmers.co.kr/learn/courses/30/lessons/43162
+function solution(n, computers) {
+  const result = [];
+  const visited = Array(n).fill(false);
+  const dfs = (arr, n) => {
+    for (let i = 0; i < computers.length; i++) {
+      if (i === n) visited[i] = true;
+      else if (!arr.includes(i) && computers[n][i] === 1) {
+        arr.push(i);
+        visited[i] = true;
+        dfs(arr, i);
+      }
+    }
+    return arr;
+  };
+  for (let i = 0; i < computers.length; i++) {
+    if (!visited[i]) result.push(dfs([i], i));
+  }
+  return result.length;
+}
