144616c0a950d416a8ff181499793923511e5d73
yoonminsang <apdlyooapdl@naver.com>
2021-10-02 16:21:49 +0900

    그리디-조이스틱

--- programmers/kit/graph.js
+++ programmers/kit/graph.js
@@ -22,3 +22,37 @@ function solution(n, edge) {
   const max = Math.max(...distArr);
   return distArr.filter((v) => v === max).length;
 }
+
+// https://programmers.co.kr/learn/courses/30/lessons/42860
+function solution(name) {
+  let count = 0;
+  let completeHead = 0,
+    completeTail = 0,
+    complete = 0;
+  let index = 0;
+  let direction = true;
+  while (name.length !== complete) {
+    const aski = name.charCodeAt(index);
+    if (aski <= 78) count += aski - 65;
+    else count += 91 - aski;
+    direction ? completeHead++ : completeTail++;
+    complete++;
+    const plusOrMinus = direction ? 1 : -1;
+    let nextA = 0;
+    let tempIndex = index + plusOrMinus;
+    while (name.charCodeAt(tempIndex) === 65) {
+      tempIndex += plusOrMinus;
+      nextA++;
+    }
+    if (nextA + complete === name.length) break;
+    if (nextA >= complete) {
+      index = direction ? name.length - completeTail - 1 : completeHead;
+      count += complete;
+      direction = !direction;
+    } else {
+      index += plusOrMinus;
+      count++;
+    }
+  }
+  return count;
+}
