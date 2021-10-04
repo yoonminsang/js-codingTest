bab44fa6fa34732c36c6b2ec391ada39c9359007
yoonminsang <apdlyooapdl@naver.com>
2021-10-04 16:04:40 +0900

    이분탐색-징검다리

--- programmers/kit/binary-search.js
+++ programmers/kit/binary-search.js
@@ -11,3 +11,40 @@ function solution(n, times) {
   }
   return l;
 }
+
+// https://programmers.co.kr/learn/courses/30/lessons/43236#
+// 못풀었음
+// https://gwang920.github.io/algorithm/progreammers-2-43236/
+function solution(distance, rocks, n) {
+  let answer = 0;
+  rocks = [0, ...rocks.sort((a, b) => a - b), distance];
+
+  const BinarySearch = () => {
+    let start = 0;
+    let end = rocks[rocks.length - 1];
+
+    while (start <= end) {
+      let mid = Math.floor((start + end) / 2);
+      let count = 0,
+        now = 0;
+      for (let i = 1; i < rocks.length; i++) {
+        if (rocks[i] - now < mid) {
+          count++;
+          if (count > n) break;
+        } else {
+          now = rocks[i];
+        }
+      }
+
+      if (count > n) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+    answer = end;
+  };
+
+  BinarySearch();
+  return answer;
+}
