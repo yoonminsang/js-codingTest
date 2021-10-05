a89846ec2ee0970fbb4693641377c15afbb760fc
yoonminsang <apdlyooapdl@naver.com>
2021-10-05 12:24:41 +0900

    해시-베스트앨범

--- ex.js
+++ ex.js
@@ -1,34 +1,17 @@
-function solution(n, results) {
-  const graph = Array(5)
-    .fill()
-    .map(() => [[], []]);
-  results.forEach(([win, lose]) => {
-    graph[win - 1][0].push(lose);
-    graph[lose - 1][1].push(win);
-  });
-  graph.forEach(([winArr, loseArr]) => {
-    winArr.forEach((v) => {
-      console.log(graph[v - 1]);
-      graph[v - 1][1] = [...new Set([...graph[v - 1][1], ...loseArr])];
-      // graph[v - 1][1] = [...new Set(graph[v - 1][1].concat(...loseArr))];
-    });
-    loseArr.forEach((v) => {
-      graph[v - 1][0] = [...new Set([...graph[v - 1][0], ...winArr])];
-      // graph[v - 1][0] = [...new Set(graph[v - 1][0].concat(...winArr))];
-    });
-  });
-  return graph.reduce((acc, cur) => {
-    if (cur.flat().length === n - 1) return acc + 1;
+function solution(genres, plays) {
+  const obj = genres.reduce((acc, genre, index) => {
+    const play = plays[index];
+    const pushArr = [play, index];
+    if (!acc[genre]) acc[genre] = [pushArr];
+    else acc[genre].push(pushArr);
     return acc;
-  }, 0);
+  });
+  console.log(obj);
 }
 
 console.log(
-  solution(5, [
-    [4, 3],
-    [4, 2],
-    [3, 2],
-    [1, 2],
-    [2, 5],
-  ])
+  solution(
+    ['classic', 'pop', 'classic', 'classic', 'pop'],
+    [500, 600, 150, 800, 2500]
+  )
 );
--- programmers/kit/hash.js
+++ programmers/kit/hash.js
@@ -19,3 +19,36 @@ function solution(clothes) {
   });
   return count - 1;
 }
+
+// https://programmers.co.kr/learn/courses/30/lessons/42579
+function solution(genres, plays) {
+  const obj = genres.reduce((acc, genre, index) => {
+    const play = plays[index];
+    const pushArr = [play, index];
+    if (!acc[genre]) acc[genre] = [pushArr];
+    else acc[genre].push(pushArr);
+    return acc;
+  }, {});
+  for (let key in obj) {
+    obj[key].sort((a, b) => {
+      if (a[0] === b[0]) return a[1] - b[1];
+      return b[0] - a[0];
+    });
+  }
+  const orderArr = [];
+  const objValues = Object.values(obj);
+  objValues.map((v, index) => {
+    orderArr.push([v.reduce((acc, [play]) => acc + play, 0), index]);
+  });
+  orderArr.sort((a, b) => b[0] - a[0]);
+  const answer = [];
+  orderArr.forEach(([_, index]) => {
+    const temp = [];
+    for (let i = 0; i < 2; i++) {
+      if (!objValues[index][i]) break;
+      temp.push(objValues[index][i][1]);
+    }
+    answer.push(...temp);
+  });
+  return answer;
+}
