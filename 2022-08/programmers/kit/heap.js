6d9066fc91c076e6d8d0eecf48b69eb68409080f
yoonminsnag <apdlyooapdl@naver.com>
2022-09-07 21:36:29 +0900

    이중우선순위큐

--- 2022-08/programmers/kit/heap.js
+++ 2022-08/programmers/kit/heap.js
@@ -19,3 +19,29 @@ function solution(jobs) {
   }
   return Math.floor(minSpendTimeArr.reduce((acc, cur) => acc + cur, 0) / minSpendTimeArr.length);
 }
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/42628
+// 이중우선순위큐
+const 최댓값_삭제 = 'D 1';
+const 최솟값_삭제 = 'D -1';
+function solution(operations) {
+  const arr = [];
+  operations.forEach((operation) => {
+    switch (operation) {
+      case 최댓값_삭제:
+        arr.pop();
+        break;
+      case 최솟값_삭제:
+        arr.shift();
+        break;
+      default:
+        const [, number] = operation.split(' ');
+        arr.push(Number(number));
+    }
+    arr.sort((a, b) => a - b);
+  });
+  if (arr.length === 0) {
+    return [0, 0];
+  }
+  return [arr[arr.length - 1], arr[0]];
+}
